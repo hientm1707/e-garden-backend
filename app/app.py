8a21e82f399a08a589c44a30d43966b313a22db2
hien.tran <hien.tran@opswat.com>
2021-06-13 09:39:47 +0700

    temp

--- app/app.py
+++ app/app.py
@@ -1,163 +1,37 @@
+from flask import Flask
+from flask_mongoengine import MongoEngine
 import yaml
 with open("db.yaml", "r") as ymlfile:
     configuration = yaml.load(ymlfile,Loader=yaml.FullLoader)
 from Adafruit_IO import MQTTClient, Client, RequestError
-from flask import Flask, flash, request, session,jsonify,make_response
+
 from flask_socketio import SocketIO
-from flask_sqlalchemy import SQLAlchemy
+
 
 '''Setups'''
 app = Flask(__name__)
 app.secret_key ="Secret Key"
-app.config['SQLALCHEMY_DATABASE_URI'] = configuration['mysql_uri']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+
+app.config['MONGODB_SETTINGS'] = {
+    "db" : "doandanganh",
+    "host" : "localhost",
+    "port":27017
+}
+db = MongoEngine()
+db.init_app(app)
+
+
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 socketio = SocketIO(app)
+#cli = pymongo.MongoClient('localhost',27017)
+#db = cli.user_login_system
+#mongo = PyMongo(app)
 # a = requests.get(url ='http://dadn.esp32thanhdanh.link/')
 # ADAFRUIT_IO_KEYBBC  = a.json().get("keyBBC")
 # ADAFRUIT_IO_KEYBBC1  = a.json().get("keyBBC1")
 # ADAFRUIT_IO_USERNAME = 'CSE_BBC'
 # ADAFRUIT_IO_USERNAME1 = 'CSE_BBC1'
 
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False,)
-    password = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __init__(self,username,password):
-        self.username = username
-        self.password =password
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
-
-@app.route('/api/account/register', methods = ['POST'])
-def register():
-    if request.method == 'POST':
-        data = request.get_json()
-        uname = data['username']
-        pword = data['password']
-        account = User.query.filter_by(username = uname, password =pword).first()
-        if account:
-            flash("Account already existed")
-            response = make_response(
-                jsonify(
-                    {"status": "false","msg": "Account already existed"}
-                ),
-                200,
-            )
-        else:
-            my_user = User(uname,pword)
-            db.session.add(my_user)
-            db.session.commit()
-            response = make_response(
-                jsonify(
-                    {"status": "true"}
-                ),
-                200,
-            )
-        response.headers["Content-Type"] = "application/json"
-        return response
-
-
-
-@app.route('/api/account/',methods = ['POST'])
-def login():
-    if request.method == 'POST':
-        data = request.get_json()
-        uname = data['username']
-        pword = data['password']
-        account = User.query.filter_by(username=uname, password=pword).first()
-        if account:
-            # db.session.configure('loggedin',True)
-            # db.session.configure('id', account['id'])
-            # db.session.configure('username', account['username'])
-            # db.session['loggedin'] = True;
-            # db.session['id'] = account['id']
-            # db.session['username'] =account['username']
-            response = make_response(
-                jsonify(
-                    {"status": "true"}
-                ),
-                200,
-            )
-        else:
-            response = make_response(
-                jsonify(
-                    {"status": "false", "msg": "Incorrect username/password"}
-                ),
-                200,
-            )
-        response.headers["Content-Type"] = "application/json"
-        return response
-
-@app.route('/api/account/logout')
-def logout():
-    session.pop('id',None)
-    session.pop('username',None)
-    session.pop('loggedin',None)
-    response = make_response(
-        jsonify(
-            {"status": "true"}
-        ),
-        200
-    )
-    response.headers["Content-Type"] = "application/json"
-    return response
-
-@app.route('/api/account/minhhientran/add',methods=['POST'])
-def subscribeToFeed():
-    if request.method =='POST':
-        data = request.get_json()
-        topic_id = data['topic_id']
-
-
-
-
-@app.route('/api/account/<username>/<feed_id>/data', methods = ['GET'])
-def getSevenNearestValue(username,feed_id):
-    restClient = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEYBBC)
-    try:
-        temperature = restClient.feeds(feed_id)
-    except RequestError:
-            response = make_response(
-                jsonify(
-                    {"status": "false", "msg": "No feed available on username"}
-                ),
-                404
-            )
-            response.headers["Content-Type"] = "application/json"
-            return response
-    listCreatedAt = [x.created_at for x in restClient.data('co2')[-7:]]
-    listValue= [x.value for x in restClient.data('co2')[-7:]]
-    if not listValue:
-        responseObj = {"status":"false","msg": "Feed has no data yet"}
-        trueResponse = make_response(
-            jsonify(
-                responseObj
-            ),
-            404
-        )
-    else:
-        listOfDicts = []
-        for i in range(len(listCreatedAt)):
-            dict = {}
-            dict['create_at'] = listCreatedAt[i]
-            dict['value'] = listValue[i]
-            listOfDicts.append(dict)
-        reponseObj = {"data":listOfDicts, "status":"true"}
-
-        trueResponse = make_response(
-            jsonify(
-                reponseObj
-            ),
-            200
-        )
-    trueResponse.headers["Content-Type"] = "application/json"
-    return trueResponse
 
 # @app.route('/index')
 # def index():
@@ -167,7 +41,133 @@ def getSevenNearestValue(username,feed_id):
 #         return json.dumps('{"status":"true"}')
 #     # User is not loggedin redirect to login page
 #     return json.dumps('{"msg":"Not authenticated"}')
+
+
+#  Routes
+# @app.route('/api/account/register', methods = ['POST'])
+# def register():
+#     if request.method == 'POST':
+#         data = request.get_json()
+#         uname = data['username']
+#         pword = data['password']
+#         account = User.query.filter_by(username = uname, password =pword).first()
+#         if account:
+#             flash("Account already existed")
+#             response = make_response(
+#                 jsonify(
+#                     {"status": "false","msg": "Account already existed"}
+#                 ),
+#                 400,
+#             )
+#         else:
+#             my_user = User(uname,pword)
+#             db.session.add(my_user)
+#             db.session.commit()
+#             response = make_response(
+#                 jsonify(
+#                     {"status": "true"}
+#                 ),
+#                 200,
+#             )
+#         response.headers["Content-Type"] = "application/json"
+#         return response
 #
+#
+#
+# @app.route('/api/account/',methods = ['POST'])
+# def login():
+#     if request.method == 'POST':
+#         data = request.get_json()
+#         uname = data['username']
+#         pword = data['password']
+#         account = User.query.filter_by(username=uname, password=pword).first()
+#         if account:
+#             # db.session.configure('loggedin',True)
+#             # db.session.configure('id', account['id'])
+#             # db.session.configure('username', account['username'])
+#             # db.session['loggedin'] = True;
+#             # db.session['id'] = account['id']
+#             # db.session['username'] =account['username']
+#             response = make_response(
+#                 jsonify(
+#                     {"status": "true"}
+#                 ),
+#                 400,
+#             )
+#         else:
+#             response = make_response(
+#                 jsonify(
+#                     {"status": "false", "msg": "Incorrect username/password"}
+#                 ),
+#                 200,
+#             )
+#         response.headers["Content-Type"] = "application/json"
+#         return response
+#
+# @app.route('/api/account/logout')
+# def logout():
+#     session.pop('id',None)
+#     session.pop('username',None)
+#     session.pop('loggedin',None)
+#     response = make_response(
+#         jsonify(
+#             {"status": "true"}
+#         ),
+#         200
+#     )
+#     response.headers["Content-Type"] = "application/json"
+#     return response
+#
+# @app.route('/api/account/minhhientran/add',methods=['POST'])
+# def subscribeToFeed():
+#     if request.method =='POST':
+#         data = request.get_json()
+#         topic_id = data['topic_id']
+#
+#
+#
+#
+# @app.route('/api/account/<username>/<feed_id>/data', methods = ['GET'])
+# def getSevenNearestValue(username,feed_id):
+#     restClient = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEYBBC)
+#     try:
+#         temperature = restClient.feeds(feed_id)
+#     except RequestError:
+#             response = make_response(
+#                 jsonify(
+#                     {"status": "false", "msg": "No feed available on username"}
+#                 ),
+#                 404
+#             )
+#             response.headers["Content-Type"] = "application/json"
+#             return response
+#     listCreatedAt = [x.created_at for x in restClient.data('co2')[-7:]]
+#     listValue= [x.value for x in restClient.data('co2')[-7:]]
+#     if not listValue:
+#         responseObj = {"status":"false","msg": "Feed has no data yet"}
+#         trueResponse = make_response(
+#             jsonify(
+#                 responseObj
+#             ),
+#             404
+#         )
+#     else:
+#         listOfDicts = []
+#         for i in range(len(listCreatedAt)):
+#             dict = {}
+#             dict['create_at'] = listCreatedAt[i]
+#             dict['value'] = listValue[i]
+#             listOfDicts.append(dict)
+#         reponseObj = {"data":listOfDicts, "status":"true"}
+#
+#         trueResponse = make_response(
+#             jsonify(
+#                 reponseObj
+#             ),
+#             200
+#         )
+#     trueResponse.headers["Content-Type"] = "application/json"
+#     return trueResponse
 @app.route('/', methods = ['GET'])
 def homepage():
     return '<p> ok </p>'
--- app/models.py
+++ app/models.py
@@ -1,1 +1,23 @@
+import uuid
+from app.app import db
+from flask import Flask,request,jsonify
 
+# class Users:
+#     def register(self):
+#         requestBody = request.get_json()
+#         user = {
+#             "_id": uuid.uuid4().hex,
+#             "username": requestBody['username']
+#             "password": requestBody['password']
+#         }
+#         # Check for existing email address
+#         if db.user.find_one({"username": user['username']}):
+#             return jsonify({"error": " Username already existed"}) , 400
+#         if db.users.insert_one(user):
+#             return jsonify(user),200
+#         return jsonify({"msg": "Signup failed", "status":"false" }),400
+
+
+class User(db.Document):
+    username = db.StringField()
+    password = db.StringField()
