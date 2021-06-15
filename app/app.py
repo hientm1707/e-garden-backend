5a1efceab4c23e8a50bb62047090fbc45e3d33d5
Hien Tran <minhhien1772000@gmail.com>
2021-06-15 14:54:27 +0700

    add subscribe, change context

--- app/app.py
+++ app/app.py
@@ -1,39 +1,42 @@
-#-----------------------FEED_ID------------------------
+# -----------------------FEED_ID------------------------
 LED_FEED = 'bk-iot-led'
 SOIL_FEED = 'bk-iot-soil'
 LIGHT_FEED = 'bk-iot-light'
 LCD_FEED = 'bk-iot-lcd'
 RELAY_FEED = 'bk-iot-relay'
-DHT11_FEED =  'bk-iot-temp-humid'
-#----------------------Base setup-----------------------
+DHT11_FEED = 'bk-iot-temp-humid'
+# ----------------------Base setup-----------------------
 from Adafruit_IO import Client, RequestError
 from flask import Flask, request, jsonify, request, session, redirect, make_response
 import yaml
 from mqtt_setup import *
+
 with open("db.yaml", "r") as ymlfile:
-    configuration = yaml.load(ymlfile,Loader=yaml.FullLoader)
+    configuration = yaml.load(ymlfile, Loader=yaml.FullLoader)
 from pymongo import MongoClient
+
 # -----------------------Warning rate------------------------
-warningRates = {'temp_rate' : 40, 'humidity_rate': 65}
+context = {'temp_rate': 40, 'humidity_rate': 65}
 
 # --------------------- MQTT Setups---------------------------------------------------------------
 import sys
+
 ADAFRUIT_IO_USERNAME = 'trminhhien17'
 ADAFRUIT_IO_KEYBBC = 'aio_Phfr33tNoyth68Tg6gWsVJXNkVbA'
 # Set to the ID of the feed to subs
 # cribe to for updates.
 
-data_for_DHT11 = {"id":"7","name":"TEMP-HUMID","data":"X","unit": "*C-%"}
+data_for_DHT11 = {"id": "7", "name": "TEMP-HUMID", "data": "X", "unit": "*C-%"}
 
-data_for_RTC = { "id" : "22","name" : "TIME","data": "x","unit":""}
-#X = 0 tat , X = 1 mo
-data_for_RELAY = {"id":"11","name":"RELAY","data":"X","unit":""}
+data_for_RTC = {"id": "22", "name": "TIME", "data": "x", "unit": ""}
+# X = 0 tat , X = 1 mo
+data_for_RELAY = {"id": "11", "name": "RELAY", "data": "X", "unit": ""}
 
-#INPUT:  X<100 toi, X>100 sang
-data_for_LIGHT ={"id":"13","name":"LIGHT","data":"X","unit":""}
+# INPUT:  X<100 toi, X>100 sang
+data_for_LIGHT = {"id": "13", "name": "LIGHT", "data": "X", "unit": ""}
 
-#X = 0 – OFF, X = 1 – RED,X = 2 – BLUE
-data_for_LED = {"id":"1","name":"LED","data":"1","unit":""}
+# X = 0 – OFF, X = 1 – RED,X = 2 – BLUE
+data_for_LED = {"id": "1", "name": "LED", "data": "1", "unit": ""}
 
 
 # Define callback functions which will be called when certain events happen.
@@ -45,9 +48,10 @@ def connected(client):
     client.subscribe(DHT11_FEED)
     client.subscribe(LIGHT_FEED)
     client.subscribe(RELAY_FEED)
-    client.subscribe('co2')
-
+    # client.subscribe('co2')
     print('Connected to Adafruit IO! Listening for changes on feeds...')
+
+
 def subscribe(client, userdata, mid, granted_qos):
     # This method is called when the client subscribes to a new feed.
 
@@ -59,11 +63,13 @@ def disconnected(client):
     print('Disconnected from Adafruit IO!')
     sys.exit(1)
 
+
 def message(client, feed_id, payload):
     # Message function will be called when a subscribed feed has a new value.
     # The feed_id parameter identifies the feed, and the payload parameter has
     # the new value.
     print('Feed {0} received new value: {1}'.format(feed_id, payload))
+
 
 def wake_up_MQTT():
     # mqttClient1 = MQTTClient(ADAFRUIT_IO_USERNAME2,ADAFRUIT_IO_KEYBBC1)
@@ -75,27 +81,31 @@ def wake_up_MQTT():
     mqttClient.connect()
     mqttClient.loop_background()
 
-#-------------------------------------------------------------------------------------------------------------
 
-#tls=True,tlsCRLFile=configuration['tlsPath']
+# -------------------------------------------------------------------------------------------------------------
+
+# tls=True,tlsCRLFile=configuration['tlsPath']
 mgClient = MongoClient(configuration['mongoRemote'])
 db = mgClient.get_database('DoAnDaNganh')
 app = Flask(__name__)
-app.secret_key ="Secret Key"
+app.secret_key = "Secret Key"
 
 mqttClient = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEYBBC)
 
 from passlib.hash import pbkdf2_sha256
 import uuid
+
+
 class User:
-    session ={'logged_in':False, 'user':None}
+    session = {'logged_in': False, 'user': None}
+
     def start_session(self, user):
         del user['password']
         session['logged_in'] = True
         session['user'] = user
         # Create an MQTT client instance.
         wake_up_MQTT()
-        return jsonify({"status":"true"}), 200
+        return jsonify({"status": "true"}), 200
 
     def signup(self):
         # Create the user object
@@ -119,7 +129,7 @@ class User:
 
     def signout(self):
         session.clear()
-        return jsonify({"status":"true"}), 200
+        return jsonify({"status": "true"}), 200
 
     def login(self):
 
@@ -134,76 +144,85 @@ class User:
 
     @staticmethod
     def publishToFeed(topic_id):
-        if 'logged_in' in session and session['logged_in']==True:
+        if 'logged_in' in session and session['logged_in'] == True:
             value = request.get_json()['value']
-            mqttClient.publish(topic_id,value)
-            return jsonify({"status":"true"})
-        return jsonify({"error": "Not authorized in"}),400
-
+            mqttClient.publish(topic_id, value)
+            return jsonify({"status": "true"})
+        return jsonify({"error": "Not authorized in"}), 400
 
     @staticmethod
     def unsubscribeFeed(topic_id):
-        if 'logged_in' in session and session['logged_in']==True:
+        if 'logged_in' in session and session['logged_in'] == True:
             mqttClient.unsubscribe(topic_id)
-            return jsonify({"status":"true","msg":"Feed {0} unsubscribed successfully".format(topic_id)}),200
-        return jsonify({"error": "Not authorized"}),400
+            return jsonify({"status": "true", "msg": "Feed {0} unsubscribed successfully".format(topic_id)}), 200
+        return jsonify({"error": "Not authorized"}), 400
+
+    @staticmethod
+    def subscribeFeed(topic_id):
+        if 'logged_in' in session and session['logged_in'] == True:
+            mqttClient.subscribe(topic_id)
+            return jsonify({"status": "true", "msg": "Feed {0} subscribed successfully".format(topic_id)}), 200
+        return jsonify({"error": "Not authorized"}), 400
 
 
-@app.route('/api/account/register', methods = ['POST'])
+@app.route('/api/account/register', methods=['POST'])
 def register():
     return User().signup()
 
-@app.route('/api/account/',methods = ['POST'])
+
+@app.route('/api/account/', methods=['POST'])
 def login():
     return User().login()
 
-@app.route('/api/account/logout',methods =['POST'])
+
+@app.route('/api/account/logout', methods=['POST'])
 def logout():
     return User().signout()
 
-@app.route('/api/account/<topic_id>/data',methods= ['GET'])
+
+@app.route('/api/account/<topic_id>/data', methods=['GET'])
 def getDataOfTopic(topic_id):
     restClient = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEYBBC)
     try:
         feed = restClient.feeds(topic_id)
     except RequestError:
-            response = make_response(
-                jsonify(
-                    {"status": "false", "msg": "No feed available on username"}
-                ),
-                404
-            )
-            response.headers["Content-Type"] = "application/json"
-            return response
+        response = make_response(
+            jsonify(
+                {"status": "false", "msg": "No feed available on username"}
+            ),
+            404
+        )
+        response.headers["Content-Type"] = "application/json"
+        return response
     data = restClient.receive(feed.key)[3]
-    return jsonify({"status":"true", "value": data}),200
+    return jsonify({"status": "true", "value": data}), 200
 
-@app.route('/api/account/minhhientran/add',methods=['POST'])
+
+@app.route('/api/account/minhhientran/add', methods=['POST'])
 def subscribeToFeed():
-    if request.method =='POST':
+    if request.method == 'POST':
         data = request.get_json()
         topic_id = data['topic_id']
 
 
-
-@app.route('/api/account/<feed_id>/seven_data', methods = ['GET'])
+@app.route('/api/account/<feed_id>/seven_data', methods=['GET'])
 def getSevenNearestValue(feed_id):
     restClient = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEYBBC)
     try:
         temperature = restClient.feeds(feed_id)
     except RequestError:
-            response = make_response(
-                jsonify(
-                    {"status": "false", "msg": "No feed available on username"}
-                ),
-                400
-            )
-            response.headers["Content-Type"] = "application/json"
-            return response
+        response = make_response(
+            jsonify(
+                {"status": "false", "msg": "No feed available on username"}
+            ),
+            400
+        )
+        response.headers["Content-Type"] = "application/json"
+        return response
     listCreatedAt = [x.created_at for x in restClient.data('co2')[-7:]]
-    listValue= [x.value for x in restClient.data('co2')[-7:]]
+    listValue = [x.value for x in restClient.data('co2')[-7:]]
     if not listValue:
-        responseObj = {"status":"false","msg": "Feed has no data yet"}
+        responseObj = {"status": "false", "msg": "Feed has no data yet"}
         trueResponse = make_response(
             jsonify(
                 responseObj
@@ -217,7 +236,7 @@ def getSevenNearestValue(feed_id):
             dict['create_at'] = listCreatedAt[i]
             dict['value'] = listValue[i]
             listOfDicts.append(dict)
-        reponseObj = {"data":listOfDicts, "status":"true"}
+        reponseObj = {"data": listOfDicts, "status": "true"}
 
         trueResponse = make_response(
             jsonify(
@@ -227,12 +246,15 @@ def getSevenNearestValue(feed_id):
         )
     trueResponse.headers["Content-Type"] = "application/json"
     return trueResponse
-@app.route('/api/account/data',methods = ['GET'])
+
+
+@app.route('/api/account/data', methods=['GET'])
 def getAllSensorsLatestData():
     restClient = Client(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEYBBC)
     feeds = restClient.feeds()
     data = [{feed.key: restClient.receive(feed.key)[3]} for feed in feeds]
-    return jsonify({"status":"true","data": data}),200
+    return jsonify({"status": "true", "data": data}), 200
+
 
 # @app.route('/index')
 # def index():
@@ -243,7 +265,7 @@ def getAllSensorsLatestData():
 #     # User is not loggedin redirect to login page
 #     return json.dumps('{"msg":"Not authenticated"}')
 #
-@app.route('/', methods = ['GET'])
+@app.route('/', methods=['GET'])
 def homepage():
     return '<p> ok </p>'
 
@@ -260,30 +282,34 @@ def modifyHumidityRate():
         if not value:
             return jsonify({"status": "false", "msg": "Invalid body format"}), 400
         if isinstance(value, int):
-            warningRates['humidity_rate'] = value
+            context['humidity_rate'] = value
             return jsonify({"status": "true"}), 200
         return jsonify({"error": "Invalid input format"}), 400
 
     else:
-        return jsonify({"rate":warningRates['humidity_rate'], "status":"true"}),200
+        return jsonify({"rate": context['humidity_rate'], "status": "true"}), 200
 
-@app.route('/api/account/unsubscribe/<topic_id>', methods = ['GET'])
+
+@app.route('/api/account/unsubscribe/<topic_id>', methods=['GET'])
 def unsubscribe(topic_id):
     return User.unsubscribeFeed(topic_id)
+@app.route('/api/account/subscribe/<topic_id>', methods=['GET'])
+def unsubscribe(topic_id):
+    return User.subscribeFeed(topic_id)
 
-@app.route('/api/account/temp_warning', methods = ['GET','PUT'])
+@app.route('/api/account/temp_warning', methods=['GET', 'PUT'])
 def modifyTempRate():
     if request.method == 'PUT':
         value = request.get_json()['value']
         if not value:
-            return jsonify({"status":"false","msg":"Invalid body format"}),400
-        if isinstance(value,int):
-            warningRates['temp_rate'] = value
-            return jsonify({"status":"true"}),200
-        return jsonify({"error":"Invalid input format"}),400
+            return jsonify({"status": "false", "msg": "Invalid body format"}), 400
+        if isinstance(value, int):
+            context['temp_rate'] = value
+            return jsonify({"status": "true"}), 200
+        return jsonify({"error": "Invalid input format"}), 400
 
     else:
-        return jsonify({"rate":warningRates['temp_rate'],"status":"true"}),200
+        return jsonify({"rate": context['temp_rate'], "status": "true"}), 200
 
 
 #
