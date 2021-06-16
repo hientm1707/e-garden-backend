c6fc376b325d997c0da28c77787e09eb57e89568
Hien Tran <minhhien1772000@gmail.com>
2021-06-16 22:51:11 +0700

    remove unneccessary files

--- app/feeds.py
+++
@@ -1,7 +0,0 @@
-class FEED_ID:
-    LED_FEED = 'bk-iot-led'
-    SOIL_FEED = 'bk-iot-soil'
-    LIGHT_FEED = 'bk-iot-light'
-    LCD_FEED = 'bk-iot-lcd'
-    RELAY_FEED = 'bk-iot-relay'
-    DHT11_FEED =  'bk-iot-temp-humid'
