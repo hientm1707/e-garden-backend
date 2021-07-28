330d4e1b1a83b721c45c81c324a08c55793499fe
hien.tran <minhhien1772000@gmail.com>
2021-07-28 21:59:38 +0700

    handle exception smtp

--- app/smtp.py
+++ app/smtp.py
@@ -2,6 +2,12 @@ import smtplib, ssl
 context = ssl.create_default_context()
 def sendEmail(sender_username, sender_password, receiver,msg):
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
-        smtp.starttls(context=context)
-        smtp.login(sender_username, sender_password)
-        smtp.sendmail(sender_username, receiver, msg)
+        try:
+            smtp.starttls(context=context)
+            smtp.login(sender_username, sender_password)
+            smtp.sendmail(sender_username, receiver, msg)
+            print('Successfully sent email')
+        except Exception as e:
+            print(e)
+
+
